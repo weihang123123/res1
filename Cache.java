89899d90babcd6743b255203f6e8a4c0a239771a
weihang123123 <57713986+weihang123123@users.noreply.github.com>
2020-02-06 10:02:15 +0800

    Create Cache.java

---
+++ Cache.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Cache {
+       private Map<String,String> map= new HashMap<String,String>();
+       publicvoid initCache()
+       {
+              map.put("Anil", "Work as Engineer");
+              map.put("Shamik", "Work as Java Engineer");
+              map.put("Ram", "Work as Doctor");
+       }
+       public Map<String,String> getCache()
+       {
+              return map;
+       }
+       publicvoid forEachDisplay()
+       {
+              for(String key : map.keySet())
+              {
+                String val = map.get(key);                 
+                System.out.println(key + " :: "+ val);
+              }
+       }
+       public static void main(String[] args) {            
+              Cache cache = new Cache();
+              cache.initCache();
+              cache.forEachDisplay();
+       }
+}
