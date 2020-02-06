93b991e8019cd47f0a8188b3dfb763af895f17e3
weihang123123 <57713986+weihang123123@users.noreply.github.com>
2020-02-06 10:00:28 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    public String name;
+     
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
+@Test
+public void givenMap_whenEqualsAndHashCodeNotOverridden_thenMemoryLeak() {
+    Map<Person, Integer> map = new HashMap<>();
+    for(int i=0; i<100; i++) {
+        map.put(new Person("jon"), 1);
+    }
+    Assert.assertFalse(map.size() == 1);
+}
